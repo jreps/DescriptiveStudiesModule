858d39ca6ea9015b1d335a275fbbd38e863397b1
Anthony Sena <asena5@its.jnj.com>
2022-08-10 16:20:23 -0400

    Bump DS package version

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -43,7 +43,7 @@ createDescriptiveStudiesModuleSpecifications <- function(
     )
   
   specifications <- list(module = "DescriptiveStudiesModule",
-                         version = "0.0.1-7",
+                         version = "0.0.1-8",
                          remoteRepo = "github.com",
                          remoteUsername = "anthonysena",
                          settings = analysis)
